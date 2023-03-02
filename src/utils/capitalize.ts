1f31ea75402c114534a8bc369cbab98a811cd87e
Nicolas <nicolasjandre@live.com>
2023-03-02 19:37:56 -0300

    created capitalize function to data treatment

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,4 +1,4 @@
 export function capitalize(str: string): string {
-    const strArray = str.split(" ");
+    const strArray = str.toLowerCase().split(" ");
     return strArray.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
