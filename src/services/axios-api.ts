210d7865a0ed448d9b4ca021452157ab2da70a73
Nicolas <nicolasjandre@live.com>
2023-03-02 16:04:34 -0300

    preparing first deploy

--- src/services/axios-api.ts
+++ src/services/axios-api.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://jandash.vercel.app/api'
 })
